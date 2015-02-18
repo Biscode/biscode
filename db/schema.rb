4c2a71bf321233782e67257e2e755fa9be3717c9
martinnabil <martin.nabil7@gmail.com>
2015-02-18 14:31:27 +0200

    i added category to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217151241) do
+ActiveRecord::Schema.define(version: 20150218120441) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150217151241) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
