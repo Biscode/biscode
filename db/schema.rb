eff3bd486375c50fef47c8122173373ca993608b
martinnabil <martin.nabil7@gmail.com>
2015-02-19 12:00:11 +0200

    i added user signature to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218205900) do
+ActiveRecord::Schema.define(version: 20150219095015) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150218205900) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "signature"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
