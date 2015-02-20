88074a8cf56fb36b47f86c0165c308706e4d0f20
HAAKS <heba.salama18@gmail.com>
2015-02-20 19:18:40 +0200

    adding profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :comments
   has_many :posts
-
+mount_uploader :image, ImageUploader
   def self.user_name(user_id)
     user = User.find(user_id)
     if user.name
